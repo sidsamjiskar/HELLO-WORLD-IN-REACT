491a306a568a6644267b277d5c743bc700c7809d
sidsamjiskar <87023564+sidsamjiskar@users.noreply.github.com>
2021-07-13 22:35:52 +0530

    REACTJS ASSIGNMENT 1 SUBMISSION - HELLO WORLD

---
+++ SRC/APP.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const Google=()=>{
+    return(
+        <div>
+            <h1 style={{ textAlign : "center"}}> Hello world </h1>
+            <h2> Hello youtubers</h2> 
+            <Button buttonText="Banana"/>
+            <Button buttonText="Mango"/>
+            <Button buttonText="strawberry"/>
+       </div>
+       
+    )
+}
+
+export default App
---
+++ SRC/INDEX.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Apple  from "./App"
+
+
+
+ReactDOM.render( <GOOGLE />, document.getElementById("root") )
